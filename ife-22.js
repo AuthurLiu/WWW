312f4a067296b7f221d3e255d7e70eaa94fa4cf0
AuthurLiu <bj2008liuzhe@126.com>
2016-11-10 17:08:25 +0800

    tree make

---
+++ ife-22.js
@@ -0,0 +1,49 @@
+var numArr = [];
+var numArrReal = [];
+(function(){
+	for(var i = 0;i<10;i++){
+		numArr[i] = i;
+	}
+	for(var j = 0,len = numArr.length;j<len;j++){
+		var random = parseInt(Math.random()*numArr.length+1);
+		numArrReal[j]=numArr[random];
+		numArr = numArr.splice(random,1);
+	}
+})
+function Node(data,left,right){
+	this.childLeft = left||null;
+	this.childRight = right||null;
+	this.data = data||null;
+}
+
+	
+		
+	
+}
+function Tree(){
+	this.root = null;
+	this.createNewTree = function(){
+		var random = parseInt(Math.random()*49+1);
+		var newNode = new Node(random);
+		if(this.root==null){
+			this.root = current;
+		}else{
+			compareNum(this.root,newNode)
+		}
+
+	};
+	this.compareNum = function(parentNode,newNode){
+		if(parentNode!=null){
+			if(newNode.data>parentNode){
+				compareNum(parentNode.childLeft,newNode);
+			}else{
+				compareNum(parentNode.childRight,newNode);
+			}
+		}else{
+			if(newNode.data>parentNode){
+				parentNode.childLeft = newNode;
+			}else{
+				parentNode.childRight = newNode;
+			}
+		}
+}
