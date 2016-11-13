35c9002f6c1053237287d5a5a6c11f004410e6d1
AuthurLiu <bj2008liuzhe@126.com>
2016-11-13 23:26:10 +0800

    home treeBi

--- ife-22.js
+++ ife-22.js
@@ -4,12 +4,12 @@ var numArrReal = [];
 	for(var i = 0;i<10;i++){
 		numArr[i] = i;
 	}
-	for(var j = 0,len = numArr.length;j<len;j++){
-		var random = parseInt(Math.random()*numArr.length+1);
+	for(var j = 0;j<10;j++){
+		var random = parseInt(Math.random()*numArr.length);
 		numArrReal[j]=numArr[random];
-		numArr = numArr.splice(random,1);
+		numArr.splice(random,1);
 	}
-})
+})()
 
 	
 		
@@ -18,35 +18,84 @@ function Node(data,left,right){
 	this.childRight = right||null;
 	this.data = data||null;
 }
+var root = null;
+var compareNum = function(parentNode,newNode){
+	
+	if(newNode.data>parentNode.data&&parentNode.childLeft!=null){
+		compareNum(parentNode.childLeft,newNode);
+	}else if(newNode.data<=parentNode.data&&parentNode.childRight!=null){
+		compareNum(parentNode.childRight,newNode);
+	}
+	
+	if(newNode.data>parentNode.data&&parentNode.childLeft==null){
+		parentNode.childLeft = newNode;
+	}else if(newNode.data<=parentNode.data&&parentNode.childRight==null){
+		parentNode.childRight = newNode;
+	}
 	
 }
-function Tree(){
-	this.root = null;
-	this.createNewTree = function(){
-		for(var i = 0,len = numArrReal.length;i<len;i++){
-			var random = numArrReal[i];
-			var newNode = new Node(random);
-			if(this.root==null){
-				this.root = newNode;
-			}else{
-				compareNum(this.root,newNode);
-			}
+var createNewTree = function(){
+	for(var i = 0,len = numArrReal.length;i<len;i++){
+		var random = numArrReal[i];
+		var newNode = new Node(random);
+		if(root==null){
+			root= newNode;
+		}else{
+			var myroot = root;
+			compareNum(myroot,newNode);
+		}
+	}
+	
+
+}
+var treeBiArr = [];
+var preOrderTraverse=function(node){
+	if(node){
+		treeBiArr.push(node.data);
+		if(node.childLeft){
+			preOrderTraverse(node.childLeft);
+		}
+		if(node.childRight){
+			preOrderTraverse(node.childRight);
 		}
 		
-
-	};
-	this.compareNum = function(parentNode,newNode){
-		if(parentNode!=null){
-			if(newNode.data>parentNode){
-				compareNum(parentNode.childLeft,newNode);
-			}else{
-				compareNum(parentNode.childRight,newNode);
-			}
-		}else{
-			if(newNode.data>parentNode){
-				parentNode.childLeft = newNode;
-			}else{
-				parentNode.childRight = newNode;
-			}
-		}
+	}
 }
+createNewTree();
+preOrderTraverse(root);
+
+// function Tree(){
+// 	this.root = null;
+// 	this.compareNum = function(parentNode,newNode){
+// 		if(parentNode!=null){
+// 			if(newNode.data>parentNode){
+// 				compareNum(parentNode.childLeft,newNode);
+// 			}else{
+// 				compareNum(parentNode.childRight,newNode);
+// 			}
+// 		}else{
+// 			if(newNode.data>parentNode){
+// 				parentNode.childLeft = newNode;
+// 			}else{
+// 				parentNode.childRight = newNode;
+// 			}
+// 		}
+// 	}
+// 	this.createNewTree = function(){
+// 		for(var i = 0,len = numArrReal.length;i<len;i++){
+// 			var random = numArrReal[i];
+// 			var newNode = new Node(random);
+// 			if(this.root==null){
+// 				this.root = newNode;
+// 			}else{
+// 				compareNum(this.root,newNode);
+// 			}
+// 		}
+		
+
+// 	}
+	
+// }
+
+// var myTree = new Tree();
+// myTree.createNewTree();
